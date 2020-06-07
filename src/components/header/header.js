672f00f507a3de592060900cb903115c0ab30bb5
mks-94 <muralikrishna9424@gmail.com>
2020-06-07 11:35:13 +0530

    Implemented redux into app and logger is useful to find redux flow

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.util";
+import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assests/crown.svg";
 import "./header.scss";
 
@@ -30,4 +31,8 @@ const Header = ({ currentUser }) => {
     </div>
   );
 };
-export default Header;
+
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(Header);

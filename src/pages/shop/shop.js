38e05e0fe767d472c21fee2cb0c58e617162aa43
mks-94 <muralikrishna9424@gmail.com>
2020-06-18 21:50:26 +0530

    Implmented state into redux selectors and stores

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,23 +1,12 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
+
 export default ShopPage;

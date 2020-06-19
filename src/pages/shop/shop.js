037849c1bfc46673c9001ef6a670206e19dfdfdf
mks-94 <muralikrishna9424@gmail.com>
2020-06-19 21:42:00 +0530

    Implemented nested routes and data normilisation

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import CollectionPage from "../collection/collection";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
+  console.log(match, "Found match prop here");
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
